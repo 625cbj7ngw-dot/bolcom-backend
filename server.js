f9c13f15bd76a72ef1920e813317ac47b0464fab
Caner <derinacosmetics@Caners-MacBook-Pro.local>
2026-05-15 00:33:23 +0200

    fix app declaration

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { Resend } = require('resend');
 const resend = new Resend(process.env.RESEND_API_KEY);
+
+const app = express();
 app.use(express.json());
 
 const CONFIG = {
