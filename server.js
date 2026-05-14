8ded859131767acad2cca29694607192208385d0
Caner <derinacosmetics@Caners-MacBook-Pro.local>
2026-05-14 20:31:01 +0200

    trigger deploy Thu May 14 20:31:01 CEST 2026

--- server.js
+++ server.js
@@ -264,3 +264,4 @@ loadDB();
 app.listen(CONFIG.PORT, () => console.log('🚀 CKTech Server v2.0 draait op poort', CONFIG.PORT));
 cron.schedule('*/5 * * * *', syncAllUsers);
 setTimeout(syncAllUsers, 3000);
+// Thu May 14 20:31:01 CEST 2026
